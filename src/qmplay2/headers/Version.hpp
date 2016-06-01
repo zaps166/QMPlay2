0018a4064cd904e873294a1e0252d1a0f3042efd
Błażej Szczygieł <spaz16@wp.pl>
2016-06-01 14:58:59 +0200

    Bump version

--- src/qmplay2/headers/Version.hpp
+++ src/qmplay2/headers/Version.hpp
@@ -1,7 +1,7 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-#define QMPlay2Version "16.05.31"
+#define QMPlay2Version "16.06.01"
 #define QMPlay2UserAgent ("QMPlay2/" QMPlay2Version)
 
 #endif // VERSION_HPP
