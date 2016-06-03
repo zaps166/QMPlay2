79e43b082eaeca4e1d9755dbb1146d56b3a100c3
Błażej Szczygieł <spaz16@wp.pl>
2016-06-03 19:23:22 +0200

    Update ChangeLog and version

--- src/qmplay2/headers/Version.hpp
+++ src/qmplay2/headers/Version.hpp
@@ -1,7 +1,7 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-#define QMPlay2Version "16.06.02"
+#define QMPlay2Version "16.06.03"
 #define QMPlay2UserAgent ("QMPlay2/" QMPlay2Version)
 
 #endif // VERSION_HPP
