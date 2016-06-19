7cc8386b22a6228cb9b1019fbb015a7ada266861
Błażej Szczygieł <spaz16@wp.pl>
2016-06-19 20:06:15 +0200

    CMake: Minor cleanups

--- src/cmake/Modules/FindSolidActions.cmake
+++ src/cmake/Modules/FindSolidActions.cmake
@@ -1,12 +1,5 @@
 # Old version for trying to find using extra-cmake-modules
 
-#find_package(ECM NO_MODULE QUIET)
-#if(ECM_FOUND)
-#    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${ECM_MODULE_PATH} ${ECM_KDE_MODULE_DIR})
-#    include(KDEInstallDirs)
-#    set(PC_INSTALL_PATH_SOLID_ACTION "/usr/${DATA_INSTALL_DIR}/solid/actions")
-#endif()
-
 if(EXISTS "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
     set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
 elseif(EXISTS "${CMAKE_INSTALL_PREFIX}/share/kde4/apps/solid/actions")
