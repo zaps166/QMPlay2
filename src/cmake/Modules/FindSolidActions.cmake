c5f797a609ec76aa2e4ad1a0209dfe3161024e43
Zamarin Arthur <arthurzam@gmail.com>
2016-06-19 13:29:07 +0300

    CMake fixes
     * POLICY CMP0043 fix
     * C++11 for Qt5 >= 5.7.0

--- src/cmake/Modules/FindSolidActions.cmake
+++ src/cmake/Modules/FindSolidActions.cmake
@@ -7,10 +7,10 @@
 #    set(PC_INSTALL_PATH_SOLID_ACTION "/usr/${DATA_INSTALL_DIR}/solid/actions")
 #endif()
 
-if(EXISTS "/usr/share/solid/actions")
+if(EXISTS "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
     set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
-elseif(EXISTS "/usr/share/kde4/apps/solid/actions")
+elseif(EXISTS "${CMAKE_INSTALL_PREFIX}/share/kde4/apps/solid/actions")
     set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/kde4/apps/solid/actions")
-elseif(EXISTS "/usr/share/apps/solid/actions")
+elseif(EXISTS "${CMAKE_INSTALL_PREFIX}/share/apps/solid/actions")
     set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/apps/solid/actions")
 endif()
