7525a254fa1632208390d6409d793836532a8689
Błażej Szczygieł <spaz16@wp.pl>
2016-06-20 19:38:56 +0200

    CMake: Small cleanup, minor fixes (#28)
    Major changes:
    - use pkg-config in XVideo module,
    - show Solid actions in summary,
    - option to set install RPATH,
    - error on OpenGL|ES 1.0.
    
    Task #22

--- src/cmake/Modules/FindGME.cmake
+++ src/cmake/Modules/FindGME.cmake
@@ -1,35 +1,27 @@
-# - Try to find LibGME
-# Once done this will define
-#  LIBGME_FOUND - System has LibGME
-#  LIBGME_INCLUDE_DIRS - The LibGME include directories
-#  LIBGME_LIBRARIES - The libraries needed to use LibGME
-#  LIBGME_DEFINITIONS - Compiler switches required for using LibGME
-#  LIBGME_LIBRARY_DIRS
+# Try to find libGME, once done this will define:
+#  - LIBGME_INCLUDE_DIRS
+#  - LIBGME_LIBRARY_DIRS
+#  - LIBGME_LIBRARIES
+#  - LIBGME_FOUND
 
-find_package(PkgConfig)
-pkg_check_modules(PC_LIBGME QUIET libgme)
-set(LIBGME_DEFINITIONS ${PC_LIBGME_CFLAGS_OTHER})
-set(LIBGME_LIBRARY_DIRS ${PC_LIBGME_LIBRARY_DIRS})
+pkg_check_modules(LIBGME QUIET libgme)
 
-if(PC_LIBGME_FOUND)
-    set(LIBGME_LIBRARIES ${PC_LIBGME_LIBRARIES})
-    set(LIBGME_INCLUDE_DIRS ${PC_LIBGME_INCLUDE_DIRS})
-    set(LIBGME_FOUND ON)
+if(LIBGME_FOUND)
+    set(LIBGME_INCLUDE_DIRS ${LIBGME_INCLUDE_DIRS})
+    set(LIBGME_LIBRARY_DIRS ${LIBGME_LIBRARY_DIRS})
+    set(LIBGME_LIBRARIES ${LIBGME_LIBRARIES})
 else()
-    find_path(LIBGME_INCLUDE_DIR gme/gme.h
-              HINTS ${PC_LIBGME_INCLUDEDIR} ${PC_LIBGME_INCLUDE_DIRS})
-
-    find_library(LIBGME_LIBRARY NAMES gme
-                 HINTS ${PC_LIBGME_LIBDIR} ${PC_LIBGME_LIBRARY_DIRS} )
+    find_path(LIBGME_INCLUDE_DIR gme/gme.h)
+    find_library(LIBGME_LIBRARY NAMES gme)
 
     include(FindPackageHandleStandardArgs)
-    # handle the QUIETLY and REQUIRED arguments and set LIBGME_FOUND to TRUE
-    # if all listed variables are TRUE
-    find_package_handle_standard_args(LibGME DEFAULT_MSG
+    # handle the QUIET and REQUIRED arguments
+    # set LIBGME_FOUND to TRUE if all listed variables are TRUE
+    find_package_handle_standard_args(LIBGME DEFAULT_MSG
                                       LIBGME_LIBRARY LIBGME_INCLUDE_DIR)
 
     mark_as_advanced(LIBGME_INCLUDE_DIR LIBGME_LIBRARY)
 
-    set(LIBGME_LIBRARIES ${LIBGME_LIBRARY})
     set(LIBGME_INCLUDE_DIRS ${LIBGME_INCLUDE_DIR})
+    set(LIBGME_LIBRARIES ${LIBGME_LIBRARY})
 endif()
