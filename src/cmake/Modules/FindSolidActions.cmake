5cf2cba8f761ece3c2fba52c8313c2af176b0ae2
Błażej Szczygieł <spaz16@wp.pl>
2016-06-20 17:10:12 +0200

    CMake: Small cleanups, minor fixes
    Major changes:
    - use pkg-config in XVideo module,
    - show Solid actions in summary,
    - option to set install RPATH,
    - error on OpenGL|ES 1.0.
    
    Task #22

--- src/cmake/Modules/FindSolidActions.cmake
+++
@@ -1,9 +0,0 @@
-# Old version for trying to find using extra-cmake-modules
-
-if(EXISTS "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
-    set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/solid/actions")
-elseif(EXISTS "${CMAKE_INSTALL_PREFIX}/share/kde4/apps/solid/actions")
-    set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/kde4/apps/solid/actions")
-elseif(EXISTS "${CMAKE_INSTALL_PREFIX}/share/apps/solid/actions")
-    set(LOCAL_INSTALL_PATH_SOLID_ACTION "${CMAKE_INSTALL_PREFIX}/share/apps/solid/actions")
-endif()
