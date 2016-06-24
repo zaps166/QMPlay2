237ca13a94c3efb5717b1269452734aec9ff36f9
Zamarin Arthur <arthurzam@gmail.com>
2016-06-24 16:17:57 +0300

    support find taglib without pkg-config

---
+++ src/cmake/Modules/FindTagLib.cmake
@@ -0,0 +1,27 @@
+# Try to find TagLib, once done this will define:
+#  - TAGLIB_INCLUDE_DIRS
+#  - TAGLIB_LIBRARY_DIRS
+#  - TAGLIB_LIBRARIES
+#  - TAGLIB_FOUND
+
+pkg_check_modules(TAGLIB QUIET taglib)
+
+if(TAGLIB_FOUND)
+    set(TAGLIB_INCLUDE_DIRS ${TAGLIB_INCLUDE_DIRS})
+    set(TAGLIB_LIBRARY_DIRS ${TAGLIB_LIBRARY_DIRS})
+    set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARIES})
+else()
+    find_path(TAGLIB_INCLUDE_DIR taglib/taglib.h PATH_SUFFIXES taglib)
+    find_library(TAGLIB_LIBRARY NAMES tag)
+
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIET and REQUIRED arguments
+    # set TAGLIB_FOUND to TRUE if all listed variables are TRUE
+    find_package_handle_standard_args(TAGLIB DEFAULT_MSG
+                                      TAGLIB_LIBRARY TAGLIB_INCLUDE_DIR)
+
+    mark_as_advanced(TAGLIB_INCLUDE_DIR TAGLIB_LIBRARY)
+
+    set(TAGLIB_INCLUDE_DIRS ${TAGLIB_INCLUDE_DIR})
+    set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARY})
+endif()
