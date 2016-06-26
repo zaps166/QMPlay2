zaps166/QMPlay2

commit 764938767881a251ef1bed52eb79ef7c203a4876

Properly find modules and languages from non-installed CMake build

src/qmplay2/QMPlay2Core.cpp
@@ -117,7 +117,7 @@ void QMPlay2CoreClass::suspend()
 #endif
 }
 
-void QMPlay2CoreClass::init(bool loadModules, const QString &libPath, const QString &sharePath, const QString &settingsPath)
+void QMPlay2CoreClass::init(bool loadModules, bool modulesInSubdirs, const QString &libPath, const QString &sharePath, const QString &settingsPath)
 {
 	if (!settingsDir.isEmpty())
 		return;
@@ -151,15 +151,21 @@ void QMPlay2CoreClass::init(bool loadModules, const QString &libPath, const QStr
 
 	if (loadModules)
 	{
-		QStringList pluginsName;
 		QFileInfoList pluginsList;
-#ifndef Q_OS_ANDROID
 		QDir(settingsDir).mkdir("Modules");
-		pluginsList += QDir(settingsDir + "Modules/").entryInfoList(QDir::Files | QDir::NoSymLinks);
-		pluginsList += QDir(libDir      + "modules/").entryInfoList(QDir::Files | QDir::NoSymLinks);
-#else
-		pluginsList += QDir(qmplay2Dir).entryInfoList(QDir::Files | QDir::NoSymLinks);
-#endif
+		pluginsList += QDir(settingsDir + "Modules/").entryInfoList(QDir::Files);
+		if (!modulesInSubdirs)
+			pluginsList += QDir(libDir + "modules/").entryInfoList(QDir::Files);
+		else
+		{
+			//Scan for modules in subdirectories - designed for CMake non-installed build
+			foreach (const QFileInfo &dInfo, QDir(libDir + "modules/").entryInfoList(QDir::Dirs))
+				foreach (const QFileInfo &fInfo, QDir(dInfo.filePath()).entryInfoList(QDir::Files))
+					if (QLibrary::isLibrary(fInfo.fileName()))
+						pluginsList += fInfo;
+		}
+
+		QStringList pluginsName;
 		foreach (const QFileInfo &fInfo, pluginsList)
 			if (QLibrary::isLibrary(fInfo.filePath()))
 			{

src/gui/Main.cpp
@@ -152,7 +152,7 @@ void QMPlay2GUIClass::runUpdate(const QString &UpdateFile)
 
 QStringList QMPlay2GUIClass::getLanguages()
 {
-	QStringList langs = QDir(langPath).entryList(QDir::NoDotAndDotDot | QDir::Files | QDir::NoSymLinks);
+	QStringList langs = QDir(langPath).entryList(QStringList() << "*.qm", QDir::NoDotAndDotDot | QDir::Files | QDir::NoSymLinks);
 	for (int i = 0; i < langs.size(); i++)
 	{
 		int idx = langs[i].indexOf('.');
@@ -475,29 +475,41 @@ int main(int argc, char *argv[])
 	}
 
 	QString libPath, sharePath = QCoreApplication::applicationDirPath();
-#if !defined Q_OS_WIN && !defined Q_OS_MAC && !defined Q_OS_ANDROID
-	sharePath += "/../share/qmplay2";
-	libPath = QMPlay2CoreClass::getLibDir();
-	if (libPath.isEmpty() || !QDir(libPath).exists("qmplay2"))
+	bool cmakeBuildFound = false;
+	if (QDir(sharePath).exists("CMakeFiles/QMPlay2.dir"))
 	{
-		libPath += "/../";
-		if (sizeof(void *) == 8 && QDir(libPath).exists("lib64/qmplay2"))
-			libPath += "lib64";
-		else if (sizeof(void *) == 4 && QDir(libPath).exists("lib32/qmplay2"))
-			libPath += "lib32";
-		else
-			libPath += "lib";
+		//We're in CMake not-installed build
+		libPath = sharePath + "/.."; //Here is "modules directory
+		sharePath += "/../.."; //Here is "lang" directory
+		if (QDir(libPath).exists("modules") || QDir(sharePath).exists("lang"))
+			cmakeBuildFound = true;
 	}
-	libPath += "/qmplay2";
+	if (!cmakeBuildFound)
+	{
+#if !defined Q_OS_WIN && !defined Q_OS_MAC && !defined Q_OS_ANDROID
+		sharePath += "/../share/qmplay2";
+		libPath = QMPlay2CoreClass::getLibDir();
+		if (libPath.isEmpty() || !QDir(libPath).exists("qmplay2"))
+		{
+			libPath += "/../";
+			if (sizeof(void *) == 8 && QDir(libPath).exists("lib64/qmplay2"))
+				libPath += "lib64";
+			else if (sizeof(void *) == 4 && QDir(libPath).exists("lib32/qmplay2"))
+				libPath += "lib32";
+			else
+				libPath += "lib";
+		}
+		libPath += "/qmplay2";
 #elif defined Q_OS_MAC
-	QString cdUp;
-	if (!QDir(sharePath).exists("share"))
-		cdUp = "/..";
-	libPath = sharePath + cdUp + "/lib/qmplay2";
-	sharePath += cdUp + "/share/qmplay2";
+		QString cdUp;
+		if (!QDir(sharePath).exists("share"))
+			cdUp = "/..";
+		libPath = sharePath + cdUp + "/lib/qmplay2";
+		sharePath += cdUp + "/share/qmplay2";
 #else
-	libPath = sharePath;
+		libPath = sharePath;
 #endif
+	}
 
 	qRegisterMetaType<VideoFrame>("VideoFrame");
 
@@ -516,7 +528,7 @@ int main(int argc, char *argv[])
 	do
 	{
 		/* QMPlay2GUI musi być stworzone już wcześniej */
-		QMPlay2Core.init(!help, libPath, sharePath);
+		QMPlay2Core.init(!help, cmakeBuildFound, libPath, sharePath);
 
 		Settings &settings = QMPlay2Core.getSettings();
 		QString lastVer = settings.getString("Version", QMPlay2Version);
@@ -529,7 +541,6 @@ int main(int argc, char *argv[])
 			settings.remove("audioWriters");
 			settings.remove("videoWriters");
 		}
-
 		if (settings.contains("Volume"))
 		{
 			const int vol = settings.getInt("Volume");

src/qmplay2/headers/QMPlay2Core.hpp
@@ -47,7 +47,7 @@ public:
 	static bool canSuspend();
 	static void suspend();
 
-	void init(bool loadModules, const QString &libPath, const QString &sharePath, const QString &settingsPath = QString());
+	void init(bool loadModules, bool modulesInSubdirs, const QString &libPath, const QString &sharePath, const QString &settingsPath = QString());
 	void quit();
 
 	inline QList<Module *> getPluginsInstance() const

commit 637a35b3d6afc7d0a5a5f2a558dcd614fcd8cc5f

Fix modules directory path for Android
Regression since 764938767881a251ef1bed52eb79ef7c203a4876

src/qmplay2/QMPlay2Core.cpp
@@ -155,7 +155,13 @@ void QMPlay2CoreClass::init(bool loadModules, bool modulesInSubdirs, const QStri
 		QDir(settingsDir).mkdir("Modules");
 		pluginsList += QDir(settingsDir + "Modules/").entryInfoList(QDir::Files);
 		if (!modulesInSubdirs)
+		{
+#ifndef Q_OS_ANDROID
 			pluginsList += QDir(libDir + "modules/").entryInfoList(QDir::Files);
+#else
+			pluginsList += QDir(libDir + "/").entryInfoList(QDir::Files);
+#endif
+		}
 		else
 		{
 			//Scan for modules in subdirectories - designed for CMake non-installed build