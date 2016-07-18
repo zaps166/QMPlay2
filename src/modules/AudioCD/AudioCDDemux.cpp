zaps166/QMPlay2

commit 0332c7c2143f2a000d2d7f41eb588903214656ee

FFmpeg: Properly handle chained OGG local files
Detect begin and end of OGG single "chain?" and simulate smaller file
size, because FFmpeg doesn't support chained OGG files properly.

src/modules/AudioCD/AudioCDDemux.cpp
@@ -282,8 +282,7 @@ Playlist::Entries AudioCDDemux::fetchTracks(const QString &url, bool &ok)
 			entries = getTracks(realUrl);
 			if (entries.isEmpty())
 				emit QMPlay2Core.sendMessage(tr("No AudioCD found!"), AudioCDName, 2, 0);
-			ok = !entries.isEmpty();
-			if (ok)
+			if (!entries.isEmpty())
 			{
 				for (int i = 0; i < entries.count(); ++i)
 					entries[i].parent = 1;
@@ -295,6 +294,7 @@ Playlist::Entries AudioCDDemux::fetchTracks(const QString &url, bool &ok)
 			}
 		}
 	}
+	ok = !entries.isEmpty();
 	return entries;
 }
 

lang/de.ts
@@ -710,6 +710,13 @@ amp</translation>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>Titel</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>System</source>

lang/es.ts
@@ -710,6 +710,13 @@ amp</translation>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>Pista</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>Copyright</source>

lang/fr.ts
@@ -709,6 +709,13 @@ amp</source>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>Piste</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>System</source>

lang/pl.ts
@@ -710,6 +710,13 @@ amp</translation>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>Ścieżka</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>System</source>

lang/ru.ts
@@ -710,6 +710,13 @@ amp</source>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>Дорожка</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>System</source>

lang/zh.ts
@@ -709,6 +709,13 @@ amp</source>
     </message>
 </context>
 <context>
+    <name>FormatContext</name>
+    <message>
+        <source>Track</source>
+        <translation>音轨</translation>
+    </message>
+</context>
+<context>
     <name>GME</name>
     <message>
         <source>System</source>
@@ -724,7 +731,7 @@ amp</source>
     </message>
     <message>
         <source>Track</source>
-        <translation type="unfinished">音轨</translation>
+        <translation>音轨</translation>
     </message>
     <message>
         <source>Voices</source>

src/modules/FFmpeg/CMakeLists.txt
@@ -20,6 +20,7 @@ set(FFmpeg_HDR
     FFReader.hpp
     FFCommon.hpp
     FormatContext.hpp
+    OggHelper.hpp
 )
 
 set(FFmpeg_SRC
@@ -30,6 +31,7 @@ set(FFmpeg_SRC
     FFReader.cpp
     FFCommon.cpp
     FormatContext.cpp
+    OggHelper.cpp
 )
 
 set(FFmpeg_RESOURCES

commit e40bec101cdc8736392d31c61606c7431c3302c7

AudioCD: Fix Windows regression after 0332c7c2143f2a000d2d7f41eb588903214656ee

src/modules/AudioCD/AudioCDDemux.cpp
@@ -23,6 +23,7 @@
 
 #ifdef Q_OS_WIN
 	#include <QDir>
+	static const QRegExp cdaRegExp("file://\\D:/track\\d\\d.cda");
 #endif
 
 #define CD_BLOCKSIZE 2352/2
@@ -198,7 +199,7 @@ void AudioCDDemux::abort()
 bool AudioCDDemux::open(const QString &_url)
 {
 #ifdef Q_OS_WIN
-	if (_url.toLower().contains(QRegExp("file://\\D:/track\\d\\d.cda")))
+	if (_url.toLower().contains(cdaRegExp))
 	{
 		QString url = _url;
 		url.remove("file://");
@@ -274,27 +275,30 @@ bool AudioCDDemux::open(const QString &_url)
 Playlist::Entries AudioCDDemux::fetchTracks(const QString &url, bool &ok)
 {
 	Playlist::Entries entries;
-	if (!Functions::splitPrefixAndUrlIfHasPluginPrefix(url, NULL, NULL, NULL))
+	if (!url.contains("://{") && url.startsWith(AudioCDName "://"))
 	{
-		if (url.startsWith(AudioCDName "://"))
+		const QString realUrl = url.mid(strlen(AudioCDName) + 3);
+		entries = getTracks(realUrl);
+		if (entries.isEmpty())
+			emit QMPlay2Core.sendMessage(tr("No AudioCD found!"), AudioCDName, 2, 0);
+		if (!entries.isEmpty())
 		{
-			const QString realUrl = url.mid(strlen(AudioCDName) + 3);
-			entries = getTracks(realUrl);
-			if (entries.isEmpty())
-				emit QMPlay2Core.sendMessage(tr("No AudioCD found!"), AudioCDName, 2, 0);
-			if (!entries.isEmpty())
-			{
-				for (int i = 0; i < entries.count(); ++i)
-					entries[i].parent = 1;
-				Playlist::Entry entry;
-				entry.name = "Audio CD (" + realUrl + ")";
-				entry.url = url;
-				entry.GID = 1;
-				entries.prepend(entry);
-			}
+			for (int i = 0; i < entries.count(); ++i)
+				entries[i].parent = 1;
+			Playlist::Entry entry;
+			entry.name = "Audio CD (" + realUrl + ")";
+			entry.url = url;
+			entry.GID = 1;
+			entries.prepend(entry);
 		}
 	}
-	ok = !entries.isEmpty();
+	if (entries.isEmpty())
+	{
+#ifdef Q_OS_WIN
+		if (!url.toLower().contains(cdaRegExp))
+#endif
+			ok = false;
+	}
 	return entries;
 }
 