zaps166/QMPlay2

commit 1229d54be5322ac7d120dd2e438cc63d75781143

Shortcuts Editor in Settings (#50)

src/gui/MenuBar.hpp
@@ -28,8 +28,6 @@ class MenuBar : public QMenuBar
 {
 	Q_OBJECT
 public:
-	static void init();
-
 	MenuBar();
 
 	class Window : public QMenu

src/gui/CMakeLists.txt
@@ -37,6 +37,8 @@ set(GUI_HDR
     Appearance.hpp
     VolWidget.hpp
     ScreenSaver.hpp
+    ShortcutHandler.hpp
+    KeyBindingsDialog.hpp
 )
 
 set(GUI_SRC
@@ -64,6 +66,8 @@ set(GUI_SRC
     Appearance.cpp
     VolWidget.cpp
     ScreenSaver.cpp
+    ShortcutHandler.cpp
+    KeyBindingsDialog.cpp
 )
 
 set(GUI_FORMS # *.ui files

src/gui/KeyBindingsDialog.cpp
@@ -1,0 +1,82 @@
+/*
+	QMPlay2 is a video and audio player.
+	Copyright (C) 2010-2016  Błażej Szczygieł
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <KeyBindingsDialog.hpp>
+#include <ShortcutHandler.hpp>
+#include <Main.hpp>
+#include <Settings.hpp>
+
+#include <QBoxLayout>
+#include <QDialogButtonBox>
+#include <QTableView>
+#include <QHeaderView>
+
+KeyBindingsDialog::KeyBindingsDialog(QWidget *p) :
+	QDialog(p)
+{
+	setWindowTitle(tr("Key Binding settings"));
+
+	QTableView *shortcuts = new QTableView(this);
+	shortcuts->setModel(ShortcutHandler::instance());
+	shortcuts->setFrameShape(QFrame::NoFrame);
+	shortcuts->setAlternatingRowColors(true);
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+	shortcuts->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+	shortcuts->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+#else
+	shortcuts->horizontalHeader()->setResizeMode(0, QHeaderView::ResizeToContents);
+	shortcuts->horizontalHeader()->setResizeMode(1, QHeaderView::Stretch);
+#endif
+	shortcuts->verticalHeader()->setVisible(false);
+
+	buttons = new QDialogButtonBox(QDialogButtonBox::Cancel | QDialogButtonBox::Apply | QDialogButtonBox::RestoreDefaults, this);
+	connect(buttons, SIGNAL(clicked(QAbstractButton*)), this, SLOT(clicked(QAbstractButton*)));
+
+	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(shortcuts);
+	layout->addWidget(buttons);
+}
+
+void KeyBindingsDialog::clicked(QAbstractButton *button)
+{
+	switch (buttons->buttonRole(button))
+	{
+		case QDialogButtonBox::ApplyRole:
+			ShortcutHandler::instance()->save();
+			break;
+		case QDialogButtonBox::RejectRole:
+			ShortcutHandler::instance()->restore();
+			break;
+		case QDialogButtonBox::ResetRole:
+			ShortcutHandler::instance()->reset();
+			break;
+		default:
+			return;
+	}
+	this->close();
+}
+
+void KeyBindingsDialog::showEvent(QShowEvent *)
+{
+	QMPlay2GUI.restoreGeometry("KeyBindingsDialog/Geometry", this, QSize(450, 450));
+}
+
+void KeyBindingsDialog::closeEvent(QCloseEvent *)
+{
+	QMPlay2Core.getSettings().set("KeyBindingsDialog/Geometry", geometry());
+}

src/gui/KeyBindingsDialog.hpp
@@ -1,0 +1,41 @@
+/*
+	QMPlay2 is a video and audio player.
+	Copyright (C) 2010-2016  Błażej Szczygieł
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef KEYBINDINGDIALOG_HPP
+#define KEYBINDINGDIALOG_HPP
+
+#include <QDialog>
+
+class QAbstractButton;
+class QDialogButtonBox;
+
+class KeyBindingsDialog : public QDialog
+{
+		Q_OBJECT
+	public:
+		KeyBindingsDialog(QWidget *p);
+	private slots:
+		void clicked(QAbstractButton *button);
+	private:
+		void showEvent(QShowEvent *);
+		void closeEvent(QCloseEvent *);
+
+		QDialogButtonBox *buttons;
+};
+
+#endif

src/gui/SettingsWidget.cpp
@@ -52,6 +52,7 @@
 #include <QDir>
 
 #include <Appearance.hpp>
+#include <KeyBindingsDialog.hpp>
 #include <Settings.hpp>
 #include <MenuBar.hpp>
 #include <Module.hpp>
@@ -202,7 +203,6 @@ void SettingsWidget::InitSettings()
 	OSDSettingsW::init("Subtitles", 20, 0, 15, 15, 15, 7, 1.5, 1.5, QColor(0xFF, 0xA8, 0x58, 0xFF), Qt::black, Qt::black);
 	OSDSettingsW::init("OSD",       32, 0, 0,  0,  0,  4, 1.5, 1.5, QColor(0xAA, 0xFF, 0x55, 0xFF), Qt::black, Qt::black);
 	DeintSettingsW::init();
-	MenuBar::init();
 	applyProxy();
 }
 void SettingsWidget::SetAudioChannelsMenu()
@@ -317,6 +317,7 @@ SettingsWidget::SettingsWidget(int page, const QString &moduleName, QWidget *vid
 		connect(page1->screenshotB, SIGNAL(clicked()), this, SLOT(chooseScreenshotDir()));
 
 		connect(page1->setAppearanceB, SIGNAL(clicked()), this, SLOT(setAppearance()));
+		connect(page1->setKeyBindingsB, SIGNAL(clicked()), this, SLOT(setKeyBindings()));
 
 #ifdef ICONS_FROM_THEME
 		page1->iconsFromTheme->setChecked(QMPSettings.getBool("IconsFromTheme"));
@@ -672,6 +673,7 @@ void SettingsWidget::chStyle()
 		QMPlay2GUI.setStyle();
 	}
 }
+
 void SettingsWidget::apply()
 {
 	Settings &QMPSettings = QMPlay2Core.getSettings();
@@ -929,6 +931,12 @@ void SettingsWidget::setAppearance()
 {
 	Appearance(this).exec();
 }
+
+void SettingsWidget::setKeyBindings()
+{
+	KeyBindingsDialog(this).exec();
+}
+
 void SettingsWidget::clearCoversCache()
 {
 	if (QMessageBox::question(this, tr("Confirm clearing the cache covers"), tr("Do you want to delete all cached covers?"), QMessageBox::Yes, QMessageBox::No) == QMessageBox::Yes)

src/gui/SettingsWidget.hpp
@@ -82,6 +82,7 @@ private slots:
 	void moveModule();
 	void chooseScreenshotDir();
 	void setAppearance();
+	void setKeyBindings();
 	void clearCoversCache();
 	void resetSettings();
 signals:

src/gui/ShortcutHandler.cpp
@@ -1,0 +1,146 @@
+/*
+	QMPlay2 is a video and audio player.
+	Copyright (C) 2010-2016  Błażej Szczygieł
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <ShortcutHandler.hpp>
+#include <QMPlay2Core.hpp>
+#include <Settings.hpp>
+
+#include <QApplication>
+
+ShortcutHandler *ShortcutHandler::s_instance = NULL;
+
+ShortcutHandler *ShortcutHandler::instance()
+{
+	if (s_instance == NULL)
+		s_instance = new ShortcutHandler;
+	return s_instance;
+}
+
+ShortcutHandler::ShortcutHandler() :
+	settings(QMPlay2Core.getSettings())
+{}
+
+ShortcutHandler::~ShortcutHandler()
+{}
+
+void ShortcutHandler::appendAction(QAction *action, const QString &settingsName, const QString &default_shortcut)
+{
+	QString shortcut = settings.getString(settingsName, default_shortcut);
+	action->setShortcut(shortcut);
+	action->setProperty("settingsId", settingsName);
+
+	m_actions.append(action);
+	m_shortcuts.insert(action, shortcut);
+	m_defaultShortcuts.insert(action, default_shortcut);
+}
+
+int ShortcutHandler::columnCount(const QModelIndex &parent) const
+{
+	Q_UNUSED(parent);
+	return 2;
+}
+
+int ShortcutHandler::rowCount(const QModelIndex &parent) const
+{
+	Q_UNUSED(parent);
+	return m_actions.count();
+}
+
+Qt::ItemFlags ShortcutHandler::flags(const QModelIndex &index) const
+{
+	switch (index.column())
+	{
+		case 0:
+			return Qt::ItemIsEnabled;
+		case 1:
+			return Qt::ItemIsEnabled | Qt::ItemIsEditable;
+	}
+	return Qt::NoItemFlags;
+}
+
+QVariant ShortcutHandler::headerData(int section, Qt::Orientation orientation, int role) const
+{
+	Q_UNUSED(orientation);
+	if (role == Qt::DisplayRole)
+	{
+		switch (section)
+		{
+			case 0:
+				return tr("Action");
+			case 1:
+				return tr("Key sequence");
+		}
+	}
+	return QVariant();
+}
+
+QVariant ShortcutHandler::data(const QModelIndex &index, int role) const
+{
+	if ((role == Qt::DisplayRole || role == Qt::EditRole) && index.row() >= 0 && index.row() < m_actions.count())
+	{
+		QAction *action = m_actions.at(index.row());
+		switch (index.column())
+		{
+			case 0:
+				return action->text().remove('&');
+			case 1:
+				return m_shortcuts.value(action);
+		}
+	}
+	return QVariant();
+}
+
+bool ShortcutHandler::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+	if (role == Qt::EditRole && index.column() == 1 && index.row() >= 0 && index.row() < m_actions.count())
+	{
+		m_shortcuts.insert(m_actions.at(index.row()), value.toString());
+		emit dataChanged(index, index);
+		return true;
+	}
+	return false;
+}
+
+void ShortcutHandler::save()
+{
+	for (Shortcuts::const_iterator iterator = m_shortcuts.constBegin(); iterator != m_shortcuts.constEnd(); ++iterator)
+	{
+		iterator.key()->setShortcut(iterator.value().trimmed());
+		settings.set(iterator.key()->property("settingsId").toString(), iterator.value());
+	}
+}
+
+void ShortcutHandler::restore()
+{
+	for (Shortcuts::iterator iterator = m_shortcuts.begin(); iterator != m_shortcuts.end(); ++iterator)
+	{
+		iterator.value() = iterator.key()->shortcut().toString();
+	}
+	emit dataChanged(createIndex(0, 1), createIndex(m_actions.count(), 1));
+}
+
+void ShortcutHandler::reset()
+{
+	for (Shortcuts::const_iterator iterator = m_defaultShortcuts.constBegin(); iterator != m_defaultShortcuts.constEnd(); ++iterator)
+	{
+		m_shortcuts.insert(iterator.key(), iterator.value());
+		iterator.key()->setShortcut(iterator.value().trimmed());
+		settings.set(iterator.key()->property("settingsId").toString(), iterator.value());
+	}
+	emit dataChanged(createIndex(0, 1), createIndex(m_actions.count(), 1));
+}

commit bd8ffc9dbddabc9df255760654df923adac9121c

Fix key shortcuts in "Widgets" menu
Also fix typo in method name.

Regression since: 1229d54be5322ac7d120dd2e438cc63d75781143

src/gui/MainWidget.cpp
@@ -316,6 +316,9 @@ MainWidget::MainWidget(QPair<QStringList, QStringList> &QMPArguments)
 	hideMenuAct->setChecked(menuHidden);
 	connect(hideMenuAct, SIGNAL(triggered(bool)), this, SLOT(hideMenu(bool)));
 	addAction(hideMenuAct);
+	QMPlay2GUI.menuBar->widgets->hideMenuAct = hideMenuAct;
+#else
+	QMPlay2GUI.menuBar->widgets->hideMenuAct = NULL;
 #endif
 
 	const bool widgetsLocked = settings.getBool("MainWidget/WidgetsLocked");
@@ -326,8 +329,9 @@ MainWidget::MainWidget(QPair<QStringList, QStringList> &QMPArguments)
 	lockWidgetsAct->setChecked(widgetsLocked);
 	connect(lockWidgetsAct, SIGNAL(triggered(bool)), this, SLOT(lockWidgets(bool)));
 	addAction(lockWidgetsAct);
+	QMPlay2GUI.menuBar->widgets->lockWidgetsAct = lockWidgetsAct;
 
-	setKeyShortcuts();
+	QMPlay2GUI.menuBar->setKeyShortcuts();
 
 	fullScreenDockWidgetState = settings.getByteArray("MainWidget/FullScreenDockWidgetState");
 #if defined Q_OS_MAC || defined Q_OS_ANDROID
@@ -385,15 +389,6 @@ MainWidget::~MainWidget()
 	emit QMPlay2Core.restoreCursor();
 	QMPlay2GUI.mainW = NULL;
 	QCoreApplication::quit();
-}
-
-void MainWidget::setKeyShortcuts()
-{
-	Settings &settings = QMPlay2Core.getSettings();
-#if !defined Q_OS_MAC && !defined Q_OS_ANDROID
-	hideMenuAct->setShortcut(settings.getString("KeyBindings/Widgets-hideMenu"));
-#endif
-	lockWidgetsAct->setShortcut(settings.getString("KeyBindings/Widgets-lockWidgets"));
 }
 
 void MainWidget::detachFromPipe()

src/gui/MainWidget.hpp
@@ -49,7 +49,6 @@ public:
 	MainWidget(QPair<QStringList, QStringList> &);
 	~MainWidget();
 private slots:
-	void setKeyShortcuts();
 	void detachFromPipe();
 
 	void focusChanged(QWidget *, QWidget *);

src/gui/MenuBar.cpp
@@ -52,7 +52,6 @@ MenuBar::MenuBar()
 #ifdef Q_OS_MAC
 	widgets->addAction(QString()); //Mac must have got at least one item inside menu, otherwise the menu is not shown (QTBUG?)
 #endif
-	setKeyShourtcuts();
 }
 
 MenuBar::Window::Window(MenuBar *parent) :
@@ -356,13 +355,19 @@ MenuBar::Help::Help(MenuBar *parent) :
 	newAction(Help::tr("About &Qt"), this, aboutQt, false, QIcon(), false);
 }
 
-void MenuBar::setKeyShourtcuts()
+void MenuBar::setKeyShortcuts()
 {
 	ShortcutHandler *shortcuts = QMPlay2GUI.shortcutHandler;
 	shortcuts->appendAction(window->toggleVisibility, "KeyBindings/Window-toggleVisibility", "`");
 	shortcuts->appendAction(window->toggleFullScreen, "KeyBindings/Window-toggleFullScreen", "F");
 	shortcuts->appendAction(window->toggleCompactView, "KeyBindings/Window-toggleCompactView", "Alt+V");
 	shortcuts->appendAction(window->close, "KeyBindings/Window-close", "Alt+F4");
+
+
+	if (widgets->hideMenuAct)
+		shortcuts->appendAction(widgets->hideMenuAct, "KeyBindings/Widgets-hideMenu", "Ctrl+Alt+M");
+	shortcuts->appendAction(widgets->lockWidgetsAct, "KeyBindings/Widgets-lockWidgets", "Shift+L");
+
 
 	shortcuts->appendAction(playlist->stopLoading, "KeyBindings/Playlist-stopLoading", "F4");
 	shortcuts->appendAction(playlist->sync, "KeyBindings/Playlist-sync", "F5");
@@ -391,6 +396,7 @@ void MenuBar::setKeyShourtcuts()
 	shortcuts->appendAction(playlist->sort->timeSort2, "KeyBindings/Playlist-Sort-timeSort2", "");
 	shortcuts->appendAction(playlist->sort->titleSort1, "KeyBindings/Playlist-Sort-titleSort1", "");
 	shortcuts->appendAction(playlist->sort->titleSort2, "KeyBindings/Playlist-Sort-titleSort2", "");
+
 
 	shortcuts->appendAction(player->togglePlay, "KeyBindings/Player-togglePlay", "Space");
 	shortcuts->appendAction(player->stop, "KeyBindings/Player-stop", "V");
@@ -448,9 +454,11 @@ void MenuBar::setKeyShourtcuts()
 	shortcuts->appendAction(playback->videoFilters->rotate90, "KeyBindings/Playback-VideoFilters-rotate90", "Ctrl+9");
 	shortcuts->appendAction(playback->videoFilters->more, "KeyBindings/Playback-VideoFilters-more", "Alt+F");
 
+
 	shortcuts->appendAction(options->settings, "KeyBindings/Options-settings", "Ctrl+O");
 	shortcuts->appendAction(options->modulesSettings, "KeyBindings/Options-modulesSettings", "Ctrl+Shift+O");
 	shortcuts->appendAction(options->trayVisible, "KeyBindings/Options-trayVisible", "Ctrl+T");
+
 
 	shortcuts->appendAction(help->about, "KeyBindings/Help-about", "F1");
 #ifdef UPDATER

src/gui/MenuBar.hpp
@@ -44,6 +44,8 @@ public:
 		friend class MenuBar;
 	public:
 		Widgets(MenuBar *parent);
+
+		QAction *hideMenuAct, *lockWidgetsAct;
 	private:
 		void menuShow();
 	};
@@ -147,7 +149,7 @@ public:
 		*aboutQt;
 	};
 
-	void setKeyShourtcuts();
+	void setKeyShortcuts();
 
 	Window *window;
 	Widgets *widgets;