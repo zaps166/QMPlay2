cd3d5d0eaec59db11d4dbc270265cac634ff2974
Błażej Szczygieł <spaz16@wp.pl>
2017-01-21 22:23:23 +0100

    Bump version

--- src/qmplay2/headers/Version.hpp
+++ src/qmplay2/headers/Version.hpp
@@ -19,7 +19,7 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-#define QMPlay2Version "17.01.20"
+#define QMPlay2Version "17.01.21"
 #define QMPlay2UserAgent ("QMPlay2/" QMPlay2Version)
 
 #endif // VERSION_HPP
