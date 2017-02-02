ff39278f9b3b2f7f52a6781363d30a23a7f964d7
Błażej Szczygieł <spaz16@wp.pl>
2017-02-02 23:39:08 +0100

    Update version and change log

--- src/qmplay2/headers/Version.hpp
+++ src/qmplay2/headers/Version.hpp
@@ -22,7 +22,7 @@
 #ifndef QMPlay2GitHEAD
 	#define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "17.01.24" QMPlay2GitHEAD
+#define QMPlay2Version "17.02.02" QMPlay2GitHEAD
 #define QMPlay2UserAgent ("QMPlay2/" QMPlay2Version)
 
 #endif // VERSION_HPP
