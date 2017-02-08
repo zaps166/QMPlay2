5e435d9c2988673a412fd12a3abdc9f2464c7746
Błażej Szczygieł <spaz16@wp.pl>
2017-02-08 19:52:14 +0100

    macOS: Fix displaying video adjustment widget action (workaround)

--- src/gui/VideoDock.cpp
+++ src/gui/VideoDock.cpp
@@ -320,7 +320,16 @@ bool VideoDock::event(QEvent *e)
 void VideoDock::popup(const QPoint &p)
 {
 	if (canPopup)
+	{
+#ifdef Q_OS_MAC
+		// Move parent of video adjustment widget from native Mac widget to QMenu
+		QWidget *videoAdj = (QWidget *)QMPlay2GUI.videoAdjustment;
+		QWidget *parent = videoAdj->parentWidget();
+		if (parent && qstrcmp(parent->metaObject()->className(), "QMacNativeWidget") == 0)
+			videoAdj->setParent(QMPlay2GUI.menuBar->playback->videoFilters->videoAdjustmentMenu);
+#endif
 		popupMenu->popup(mapToGlobal(p));
+	}
 }
 void VideoDock::hideCursor()
 {
