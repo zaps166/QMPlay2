b7e9219666747adf90c4bcade929a29b8fa5aa7a
Błażej Szczygieł <spaz16@wp.pl>
2017-03-25 22:28:41 +0100

    IPC: Use "u_long" for "ioctl"

--- src/qmplay2/IPC_Unix.cpp
+++ src/qmplay2/IPC_Unix.cpp
@@ -98,7 +98,8 @@ bool IPCSocket::open(QIODevice::OpenMode mode)
 
 	if (m_priv->fd > 0)
 	{
-		ioctl(m_priv->fd, FIONBIO, "\xFF\xFF\xFF\xFF");
+		const u_long on = 1;
+		ioctl(m_priv->fd, FIONBIO, &on);
 		m_priv->socketNotifier = new QSocketNotifier(m_priv->fd, QSocketNotifier::Read, this);
 		connect(m_priv->socketNotifier, SIGNAL(activated(int)), this, SLOT(socketReadActive()));
 		return QIODevice::open(mode);
