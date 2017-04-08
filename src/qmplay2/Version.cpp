9a840cb7f120f6ed821e6a4f4ad0f08743df9e11
Błażej Szczygieł <spaz16@wp.pl>
2017-04-08 13:40:22 +0200

    Update changelog and version

--- src/qmplay2/Version.cpp
+++ src/qmplay2/Version.cpp
@@ -25,7 +25,7 @@
 #ifndef QMPlay2GitHEAD
 	#define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "17.04.05" QMPlay2GitHEAD
+#define QMPlay2Version "17.04.08" QMPlay2GitHEAD
 
 QByteArray Version::get()
 {
