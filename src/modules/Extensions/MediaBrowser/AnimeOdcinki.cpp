6e52ba026b8fa9f528d3248a9fa42a97cc59109a
Błażej Szczygieł <spaz16@wp.pl>
2017-10-13 16:50:24 +0200

    AnimeOdcinki: Change player priority, minor changes
    Google works correctly in youtube-dl now.

--- src/modules/Extensions/MediaBrowser/AnimeOdcinki.cpp
+++ src/modules/Extensions/MediaBrowser/AnimeOdcinki.cpp
@@ -146,10 +146,10 @@ static EmbeddedPlayers getEmbeddedPlayers(const QByteArray &data)
 				const bool isVk       = (name == "vk");
 				const bool isVIDFile  = (name == "vidfile");
 
-				if (isGoogle || isOpenload)
-					ret.insert(ret.end(), json.object());
-				else if (isVk || isVIDFile)
+				if (isOpenload || isVIDFile)
 					ret.push_back(json.object());
+				else if (isVk || isGoogle)
+					ret.insert(ret.begin(), json.object());
 			}
 		}
 
@@ -346,6 +346,7 @@ bool AnimeOdcinki::convertAddress(const QString &prefix, const QString &url, con
 			};
 
 			const auto getDownloadButtonUrl = [&](bool allowGDriveRawFile) {
+				// Might not work due to linkexpander problems.
 				const QByteArray adFlyUrl = getAdFlyUrl(reply).toPercentEncoding();
 				if (!adFlyUrl.isEmpty() && net.startAndWait(netReply, g_linkexpander, "url=" + adFlyUrl, NetworkAccess::UrlEncoded, 3))
 				{
@@ -353,7 +354,7 @@ bool AnimeOdcinki::convertAddress(const QString &prefix, const QString &url, con
 					const int idx = data.indexOf("<");
 					if (idx > -1)
 					{
-						const QString &animeUrl = data.left(idx);
+						const QString animeUrl = data.left(idx);
 						if (!allowGDriveRawFile || !animeUrl.contains("docs.google.com"))
 							hasStreamUrl = getStreamUrl(animeUrl);
 						else if (net.startAndWait(netReply, animeUrl))
