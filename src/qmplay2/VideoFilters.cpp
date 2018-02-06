e95458763386034542d6538d92422e4c0b38bc6b
Błażej Szczygieł <spaz16@wp.pl>
2018-02-06 23:19:25 +0100

    Update copyright year

--- src/qmplay2/VideoFilters.cpp
+++ src/qmplay2/VideoFilters.cpp
@@ -1,6 +1,6 @@
 /*
 	QMPlay2 is a video and audio player.
-	Copyright (C) 2010-2017  Błażej Szczygieł
+	Copyright (C) 2010-2018  Błażej Szczygieł
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Lesser General Public License as published
