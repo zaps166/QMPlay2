8bb577b51c689eea109840b5be5f65dbe59fb73a
Błażej Szczygieł <spaz16@wp.pl>
2018-02-07 19:28:26 +0100

    libmodplug: Add missing {} in virtual destructor

--- src/modules/Modplug/libmodplug/sndfile.hpp
+++ src/modules/Modplug/libmodplug/sndfile.hpp
@@ -453,7 +453,7 @@ typedef struct _MODCOMMAND
 class IMixPlugin
 {
 public:
-	virtual ~IMixPlugin();
+	virtual ~IMixPlugin() {}
 	virtual int AddRef() = 0;
 	virtual int Release() = 0;
 	virtual void SaveAllParameters() = 0;
