97e85914b431f054334fd4a3520595e50dce7d65
Błażej Szczygieł <spaz16@wp.pl>
2018-02-12 20:53:33 +0100

    Update change log and version

--- src/qmplay2/Version.cpp
+++ src/qmplay2/Version.cpp
@@ -25,7 +25,7 @@
 #ifndef QMPlay2GitHEAD
 	#define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "18.02.05" QMPlay2GitHEAD
+#define QMPlay2Version "18.02.12" QMPlay2GitHEAD
 
 QByteArray Version::get()
 {
