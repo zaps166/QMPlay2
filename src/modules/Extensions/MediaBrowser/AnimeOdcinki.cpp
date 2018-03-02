5425b3e05f04063721efdb176d2f87dc47632306
Błażej Szczygieł <spaz16@wp.pl>
2018-03-02 20:08:27 +0100

    Update linkexpanded URL (does it work?)

--- src/modules/Extensions/MediaBrowser/AnimeOdcinki.cpp
+++ src/modules/Extensions/MediaBrowser/AnimeOdcinki.cpp
@@ -32,7 +32,7 @@
 Q_LOGGING_CATEGORY(animeodcinki, "AnimeOdcinki")
 
 constexpr const char *g_url = "https://a-o.ninja/anime/";
-constexpr const char *g_linkexpander = "http://www.linkexpander.com/get_url.php";
+constexpr const char *g_linkexpander = "https://linkexpander.com/get_url.php";
 constexpr const char *g_serverPrioritiesUrl = "https://raw.githubusercontent.com/zaps166/QMPlay2OnlineContents/master/AnimeOdcinki.json";
 
 static AnimeOdcinki::AnimePairList parseAnimeList(const QByteArray &data, AnimeOdcinki::AnimePair *episodeImgDescr)
