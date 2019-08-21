011bfb1927cc1f120735f3c7e5db34dcdd7fdaa7
Błażej Szczygieł <spaz16@wp.pl>
2019-08-21 23:21:10 +0200

    VDPAU: Remove wrong comment

--- src/modules/FFmpeg/FFDecVDPAU.cpp
+++ src/modules/FFmpeg/FFDecVDPAU.cpp
@@ -75,7 +75,7 @@ bool FFDecVDPAU::open(StreamInfo &streamInfo, VideoWriter *writer)
                 new HWAccelHelper(codec_ctx, AV_PIX_FMT_VDPAU, vdpauCtx, vdpauWriter->getSurfacesQueue());
 
                 if (pix_fmt == AV_PIX_FMT_YUVJ420P)
-                    codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P; //Force full color range
+                    codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
 
                 if (openCodec(codec))
                 {
