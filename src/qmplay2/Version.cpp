2b97772a145671c0c64406ed52aec2f3e7fbcc7e
Błażej Szczygieł <spaz16@wp.pl>
2019-11-26 19:55:38 +0100

    Bump version to 19.11.26

--- src/qmplay2/Version.cpp
+++ src/qmplay2/Version.cpp
@@ -25,7 +25,7 @@
 #ifndef QMPlay2GitHEAD
     #define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "19.11.06" QMPlay2GitHEAD
+#define QMPlay2Version "19.11.26" QMPlay2GitHEAD
 
 QByteArray Version::get()
 {
