c6944155474e636119078650a32d62b62440de2b
Błażej Szczygieł <spaz16@wp.pl>
2020-01-01 13:43:11 +0100

    Happy New Year (2020)!

--- src/modules/FileAssociation/FileAssociation.hpp
+++ src/modules/FileAssociation/FileAssociation.hpp
@@ -1,6 +1,6 @@
 /*
     QMPlay2 is a video and audio player.
-    Copyright (C) 2010-2019  Błażej Szczygieł
+    Copyright (C) 2010-2020  Błażej Szczygieł
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
