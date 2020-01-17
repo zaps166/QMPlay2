a9af85b093a3bdd6ef530ccc1f5cac31c4699c43
Błażej Szczygieł <spaz16@wp.pl>
2020-01-17 19:22:00 +0100

    VDPAU: Move VDPAUOpenGL to separate files

---
+++ src/modules/FFmpeg/VDPAUOpenGL.cpp
@@ -0,0 +1,234 @@
+/*
+    QMPlay2 is a video and audio player.
+    Copyright (C) 2010-2020  Błażej Szczygieł
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <VDPAUOpenGL.hpp>
+#include <FFCommon.hpp>
+
+#include <QMPlay2Core.hpp>
+
+#include <QDebug>
+#include <QSet>
+
+VDPAUOpenGL::VDPAUOpenGL(const std::shared_ptr<VDPAU> &vdpau)
+    : m_vdpau(vdpau)
+{}
+VDPAUOpenGL::~VDPAUOpenGL()
+{}
+
+QString VDPAUOpenGL::name() const
+{
+    return VDPAUWriterName;
+}
+
+HWAccelInterface::Format VDPAUOpenGL::getFormat() const
+{
+    return RGB32;
+}
+
+bool VDPAUOpenGL::init(const int *widths, const int *heights, const HWAccelInterface::SetTextureParamsFn &setTextureParamsFn)
+{
+    Q_UNUSED(widths)
+    Q_UNUSED(heights)
+
+    m_setTextureParamsFn = setTextureParamsFn;
+
+    m_vdpau->m_outputSurfacesMutex.lock();
+    clearObsoleteSurfaces();
+    for (auto &&outputSurfacePair : m_vdpau->m_outputSurfacesMap)
+    {
+        auto &outputSurface = outputSurfacePair.second;
+        if (outputSurface.glTexture != 0)
+            m_setTextureParamsFn(outputSurface.glTexture);
+    }
+    m_vdpau->m_outputSurfacesMutex.unlock();
+
+    if (m_isInitialized)
+        return true;
+
+    const auto context = QOpenGLContext::currentContext();
+    if (!context)
+    {
+        QMPlay2Core.logError("VDPAU :: Unable to get OpenGL context");
+        return false;
+    }
+
+    if (!context->extensions().contains("GL_NV_vdpau_interop"))
+    {
+        QMPlay2Core.logError("VDPAU :: GL_NV_vdpau_interop extension is not available");
+        return false;
+    }
+
+    VDPAUInitNV = (PFNVDPAUInitNVPROC)context->getProcAddress("VDPAUInitNV");
+    VDPAUFiniNV = (PFNVDPAUFiniNVPROC)context->getProcAddress("VDPAUFiniNV");
+    VDPAURegisterOutputSurfaceNV = (PFNVDPAURegisterSurfaceNVPROC)context->getProcAddress("VDPAURegisterOutputSurfaceNV");
+    VDPAUUnregisterSurfaceNV = (PFNVDPAUUnregisterSurfaceNVPROC)context->getProcAddress("VDPAUUnregisterSurfaceNV");
+    VDPAUSurfaceAccessNV = (PFNVDPAUSurfaceAccessNVPROC)context->getProcAddress("VDPAUSurfaceAccessNV");
+    VDPAUMapSurfacesNV = (PFNVDPAUMapUnmapSurfacesNVPROC)context->getProcAddress("VDPAUMapSurfacesNV");
+    VDPAUUnmapSurfacesNV = (PFNVDPAUMapUnmapSurfacesNVPROC)context->getProcAddress("VDPAUUnmapSurfacesNV");
+    if (!VDPAUInitNV || !VDPAUFiniNV || !VDPAURegisterOutputSurfaceNV || !VDPAUUnregisterSurfaceNV || !VDPAUSurfaceAccessNV || !VDPAUMapSurfacesNV || !VDPAUUnmapSurfacesNV)
+    {
+        QMPlay2Core.logError("VDPAU :: Unable to get VDPAU interop function pointers");
+        return false;
+    }
+
+    VDPAUInitNV(m_vdpau->m_device, m_vdpau->vdp_get_proc_address);
+    if (glGetError() != 0)
+    {
+        QMPlay2Core.logError("VDPAU :: Unable to initialize VDPAU <-> GL interop");
+        return false;
+    }
+
+    m_isInitialized = true;
+    return true;
+}
+void VDPAUOpenGL::clear()
+{
+    clearSurfaces();
+    m_setTextureParamsFn = nullptr;
+
+    if (!m_isInitialized)
+        return;
+
+    VDPAUFiniNV();
+
+    VDPAUInitNV = nullptr;
+    VDPAUFiniNV = nullptr;
+    VDPAURegisterOutputSurfaceNV = nullptr;
+    VDPAUUnregisterSurfaceNV = nullptr;
+    VDPAUSurfaceAccessNV = nullptr;
+    VDPAUMapSurfacesNV = nullptr;
+    VDPAUUnmapSurfacesNV = nullptr;
+
+    m_isInitialized = false;
+}
+
+HWAccelInterface::MapResult VDPAUOpenGL::mapFrame(Frame &videoFrame)
+{
+    QMutexLocker locker(&m_vdpau->m_outputSurfacesMutex);
+
+    clearObsoleteSurfaces();
+
+    if (auto displayingOutputSurface = m_vdpau->getDisplayingOutputSurface())
+    {
+        VDPAUUnmapSurfacesNV(1, &displayingOutputSurface->glSurface);
+        displayingOutputSurface->displaying = false;
+    }
+
+    auto it = m_vdpau->m_outputSurfacesMap.find(videoFrame.customID());
+    if (it == m_vdpau->m_outputSurfacesMap.end())
+        return MapNotReady;
+
+    auto &outputSurface = it->second;
+
+    videoFrame.setOnDestroyFn(nullptr);
+    outputSurface.busy = false;
+
+    if (outputSurface.glTexture == 0)
+    {
+        glGenTextures(1, &outputSurface.glTexture);
+        m_setTextureParamsFn(outputSurface.glTexture);
+    }
+
+    if (outputSurface.glSurface == 0)
+    {
+        outputSurface.glSurface = VDPAURegisterOutputSurfaceNV(outputSurface.surface, GL_TEXTURE_2D, 1, &outputSurface.glTexture);
+        if (outputSurface.glSurface == 0)
+            return MapError;
+        VDPAUSurfaceAccessNV(outputSurface.glSurface, GL_READ_ONLY);
+    }
+
+    Q_ASSERT(!outputSurface.displaying);
+    VDPAUMapSurfacesNV(1, &outputSurface.glSurface);
+    if (glGetError() != 0)
+        return MapError;
+    outputSurface.displaying = true;
+
+    return MapOk;
+}
+quint32 VDPAUOpenGL::getTexture(int plane)
+{
+    Q_UNUSED(plane)
+    QMutexLocker locker(&m_vdpau->m_outputSurfacesMutex);
+    if (auto displayingOutputSurface = m_vdpau->getDisplayingOutputSurface())
+        return displayingOutputSurface->glTexture;
+    return 0;
+}
+
+bool VDPAUOpenGL::getImage(const Frame &videoFrame, void *dest, ImgScaler *nv12ToRGB32)
+{
+    Q_UNUSED(nv12ToRGB32) // FIXME: Don't use ImgScaler in VideoThr if not needed
+    return m_vdpau->getRGB((uint8_t *)dest, videoFrame.width(), videoFrame.height());
+}
+
+void VDPAUOpenGL::getVideAdjustmentCap(VideoAdjustment &videoAdjustmentCap)
+{
+    videoAdjustmentCap.brightness = false;
+    videoAdjustmentCap.contrast = false;
+    videoAdjustmentCap.saturation = true;
+    videoAdjustmentCap.hue = true;
+    videoAdjustmentCap.sharpness = true;
+}
+void VDPAUOpenGL::setVideoAdjustment(const VideoAdjustment &videoAdjustment)
+{
+    m_vdpau->applyVideoAdjustment(videoAdjustment.saturation, videoAdjustment.hue, videoAdjustment.sharpness);
+}
+
+void VDPAUOpenGL::clearSurfaces()
+{
+    QMutexLocker locker(&m_vdpau->m_outputSurfacesMutex);
+    for (auto &&outputSurfacePair : m_vdpau->m_outputSurfacesMap)
+        deleteGlSurface(outputSurfacePair.second);
+    clearObsoleteSurfaces();
+}
+
+void VDPAUOpenGL::clearObsoleteSurfaces()
+{
+    for (auto it = m_vdpau->m_outputSurfacesMap.begin(); it != m_vdpau->m_outputSurfacesMap.end();)
+    {
+        auto &outputSurface = it->second;
+        if (outputSurface.obsolete && !outputSurface.busy && !outputSurface.displaying)
+        {
+            deleteGlSurface(outputSurface);
+            m_vdpau->vdp_output_surface_destroy(outputSurface.surface);
+            it = m_vdpau->m_outputSurfacesMap.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
+
+void VDPAUOpenGL::deleteGlSurface(VDPAUOutputSurface &surface)
+{
+    if (surface.displaying)
+    {
+        VDPAUUnmapSurfacesNV(1, &surface.glSurface);
+        surface.displaying = false;
+    }
+    if (surface.glSurface != 0)
+    {
+        VDPAUUnregisterSurfaceNV(surface.glSurface);
+        surface.glSurface = 0;
+    }
+    if (surface.glTexture != 0)
+    {
+        glDeleteTextures(1, &surface.glTexture);
+        surface.glTexture = 0;
+    }
+}
---
+++ src/modules/FFmpeg/VDPAUOpenGL.hpp
@@ -0,0 +1,87 @@
+/*
+    QMPlay2 is a video and audio player.
+    Copyright (C) 2010-2020  Błażej Szczygieł
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <HWAccelInterface.hpp>
+#include <VDPAU.hpp>
+
+#include <QOpenGLContext>
+
+class VDPAUOpenGL : public HWAccelInterface
+{
+public:
+    VDPAUOpenGL(const std::shared_ptr<VDPAU> &vdpau);
+    ~VDPAUOpenGL() final;
+
+    QString name() const override;
+
+    Format getFormat() const override;
+
+    bool init(const int *widths, const int *heights, const SetTextureParamsFn &setTextureParamsFn) override;
+    void clear() override;
+
+    MapResult mapFrame(Frame &videoFrame) override;
+    quint32 getTexture(int plane) override;
+
+    bool getImage(const Frame &videoFrame, void *dest, ImgScaler *nv12ToRGB32) override;
+
+    void getVideAdjustmentCap(VideoAdjustment &videoAdjustmentCap) override;
+    void setVideoAdjustment(const VideoAdjustment &videoAdjustment) override;
+
+    /**/
+
+    inline std::shared_ptr<VDPAU> getVDPAU() const
+    {
+        return m_vdpau;
+    }
+
+private:
+    void clearSurfaces();
+
+    void clearObsoleteSurfaces();
+
+    void deleteGlSurface(VDPAUOutputSurface &surface);
+
+private:
+    using GLvdpauSurfaceNV = GLintptr;
+
+    std::shared_ptr<VDPAU> m_vdpau;
+
+    bool m_isInitialized = false;
+    SetTextureParamsFn m_setTextureParamsFn;
+
+    using PFNVDPAUInitNVPROC = void(*)(uintptr_t vdpDevice, VdpGetProcAddress getProcAddress);
+    PFNVDPAUInitNVPROC VDPAUInitNV = nullptr;
+
+    using PFNVDPAUFiniNVPROC = void(*)();
+    PFNVDPAUFiniNVPROC VDPAUFiniNV = nullptr;
+
+    using PFNVDPAURegisterSurfaceNVPROC = GLvdpauSurfaceNV(*)(uintptr_t vdpSurface, GLenum target, GLsizei numTextureNames, const GLuint *textureNames);
+    PFNVDPAURegisterSurfaceNVPROC VDPAURegisterOutputSurfaceNV = nullptr;
+
+    using PFNVDPAUUnregisterSurfaceNVPROC = void(*)(GLvdpauSurfaceNV surface);
+    PFNVDPAUUnregisterSurfaceNVPROC VDPAUUnregisterSurfaceNV = nullptr;
+
+    using PFNVDPAUSurfaceAccessNVPROC = void(*)(GLvdpauSurfaceNV surface, GLenum access);
+    PFNVDPAUSurfaceAccessNVPROC VDPAUSurfaceAccessNV = nullptr;
+
+    using PFNVDPAUMapUnmapSurfacesNVPROC = void(*)(GLsizei numSurfaces, const GLvdpauSurfaceNV *surfaces);
+    PFNVDPAUMapUnmapSurfacesNVPROC VDPAUMapSurfacesNV = nullptr;
+    PFNVDPAUMapUnmapSurfacesNVPROC VDPAUUnmapSurfacesNV = nullptr;
+};
