a99c45170986b75a6e4bcbcebfb0a648c37bf493
Błażej Szczygieł <spaz16@wp.pl>
2020-02-07 21:35:31 +0100

    VideoOutputCommon: Fix rotation of 90 degrees

--- src/qmplay2/VideoOutputCommon.cpp
+++ src/qmplay2/VideoOutputCommon.cpp
@@ -71,7 +71,7 @@ void VideoOutputCommon::setSphericalView(bool sphericalView)
 void VideoOutputCommon::updateSizes(const QSizeF &size, bool transpose)
 {
     m_scaledSize = m_zoom * (transpose
-        ? QSizeF(1.0, m_aRatio).scaled(size.transposed(), Qt::KeepAspectRatio)
+        ? QSizeF(1.0, m_aRatio).scaled(size, Qt::KeepAspectRatio)
         : QSizeF(m_aRatio, 1.0).scaled(size, Qt::KeepAspectRatio)
     );
 
