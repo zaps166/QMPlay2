4b8b66a0eee8190d3c7dde6e31890a0bac07d4be
Błażej Szczygieł <spaz16@wp.pl>
2020-02-25 01:33:17 +0100

    Frame: Fix old FFmpeg compatibility

--- src/qmplay2/Frame.cpp
+++ src/qmplay2/Frame.cpp
@@ -49,9 +49,11 @@ AVPixelFormat Frame::convert3PlaneTo2Plane(AVPixelFormat fmt)
         case AV_PIX_FMT_YUV422P10:
             return AV_PIX_FMT_NV20;
 
+#if LIBAVUTIL_VERSION_INT >= AV_VERSION_INT(56, 31, 100)
         case AV_PIX_FMT_YUV444P:
         case AV_PIX_FMT_YUVJ444P:
             return AV_PIX_FMT_NV24;
+#endif
 
         default:
             break;
@@ -74,8 +76,10 @@ AVPixelFormat Frame::convert2PlaneTo3Plane(AVPixelFormat fmt)
         case AV_PIX_FMT_NV20:
             return AV_PIX_FMT_YUV422P10;
 
+#if LIBAVUTIL_VERSION_INT >= AV_VERSION_INT(56, 31, 100)
         case AV_PIX_FMT_NV24:
             return AV_PIX_FMT_YUV444P;
+#endif
 
         default:
             break;
