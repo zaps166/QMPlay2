8b1dd3ff256fa78f925e2579337bf961b88d0ec1
Błażej Szczygieł <spaz16@wp.pl>
2020-04-19 11:17:00 +0200

    DXVA2: Compare AV_PIX_FMT_P016

--- src/modules/FFmpeg/FFDecDXVA2.cpp
+++ src/modules/FFmpeg/FFDecDXVA2.cpp
@@ -125,7 +125,7 @@ void FFDecDXVA2::downloadVideoFrame(Frame &decoded)
 bool FFDecDXVA2::open(StreamInfo &streamInfo)
 {
     m_pixFmt = Frame::convert3PlaneTo2Plane(streamInfo.pixelFormat());
-    if (m_pixFmt != AV_PIX_FMT_NV12 && m_pixFmt != AV_PIX_FMT_P010)
+    if (m_pixFmt != AV_PIX_FMT_NV12 && m_pixFmt != AV_PIX_FMT_P016)
         return false;
 
     AVCodec *codec = init(streamInfo);
