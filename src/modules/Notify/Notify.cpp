a3ab65672f9fb2d5c852245c5d8ff13eb57d05eb
Błażej Szczygieł <spaz16@wp.pl>
2022-01-01 02:44:38 +0100

    Update copyright year

--- src/modules/Notify/Notify.cpp
+++ src/modules/Notify/Notify.cpp
@@ -1,6 +1,6 @@
 ﻿/*
     QMPlay2 is a video and audio player.
-    Copyright (C) 2010-2021  Błażej Szczygieł
+    Copyright (C) 2010-2022  Błażej Szczygieł
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
