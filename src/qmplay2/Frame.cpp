2b07cdaa5cefc844c87aeca6e2990134b48594d8
Błażej Szczygieł <mumei6102@gmail.com>
2023-08-04 18:11:19 +0200

    Frame: Add interlaced info to downloaded frame

--- src/qmplay2/Frame.cpp
+++ src/qmplay2/Frame.cpp
@@ -528,6 +528,7 @@ Frame Frame::downloadHwData() const
         if (av_hwframe_transfer_data(dstFrame, m_frame, 0) == 0)
         {
             downloaded = Frame(dstFrame);
+            downloaded.setInterlaced(isTopFieldFirst());
             downloaded.setTimeBase(m_timeBase);
             downloaded.setTSInt(tsInt());
         }
