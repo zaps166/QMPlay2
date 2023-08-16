f48fcd5b9af6c94098d6e981cb344fbd463e8eee
Błażej Szczygieł <mumei6102@gmail.com>
2023-08-17 00:18:37 +0200

    Yadif: Remove inline assembly and optimize C++ code

--- src/modules/VideoFilters/YadifDeint.cpp
+++ src/modules/VideoFilters/YadifDeint.cpp
@@ -19,37 +19,18 @@
 /*
     This is the C++/Qt port of the Yadif deinterlacing filter from FFmpeg (libavfilter/vf_yadif.c)
     Copyright (C) 2006-2011 Michael Niedermayer <michaelni@gmx.at>
-
-    The Yadif x86 SIMD implementation is taken from:
-    - FFmpeg project (libavfilter/x86/vf_yadif.asm)
-        Copyright (C) 2006-2011 Michael Niedermayer <michaelni@gmx.at>
-    - MPlayer project (libmpcodecs/vf_yadif.c)
-        Copyright (C) 2006 Michael Niedermayer <michaelni@gmx.at>
 */
 
 #include <YadifDeint.hpp>
 
 #include <QMPlay2Core.hpp>
-#include <CPU.hpp>
 
 #include <QtConcurrent/QtConcurrentRun>
-
-extern "C" {
-    #include <libavutil/cpu.h>
-}
 
 #include <algorithm>
 #include <vector>
 
-#undef RSHIFT
-
 using namespace std;
-
-static void (*filterLinePtr)(quint8 *, const void *const,
-                             const quint8 *, const quint8 *, const quint8 *,
-                             const qptrdiff, const qptrdiff,
-                             const int, const bool);
-static int alignment;
 
 /* Yadif algo */
 
@@ -61,7 +42,12 @@ static inline void check(const quint8 *const curr,
     const int score = abs(curr[mrefs-1+j] - curr[prefs-1-j]) + abs(curr[mrefs+j] - curr[prefs-j]) + abs(curr[mrefs+1+j] - curr[prefs+1-j]);
     if (score < spatialScore)
     {
+        // This must be in a separate condition w/o assigning condition to a variable,
+        // otherwise the compiler will not optimize it.
         spatialScore = score;
+    }
+    if (score < spatialScore)
+    {
         spatialPred = (curr[mrefs+j] + curr[prefs-j]) >> 1;
         switch (j)
         {
@@ -116,7 +102,7 @@ static inline void filterLine(quint8 *dest, const void *const destEnd,
 
         if (spatialPred > d + diff)
             spatialPred = d + diff;
-        else if (spatialPred < d - diff)
+        if (spatialPred < d - diff)
             spatialPred = d - diff;
 
         *(dest++) = spatialPred;
@@ -126,287 +112,6 @@ static inline void filterLine(quint8 *dest, const void *const destEnd,
         ++prev2;
         ++next2;
     }
-}
-
-#ifdef QMPLAY2_CPU_X86
-
-#define LOAD(mem, dst) \
-    _movh "    " mem  ", " dst"\n\t"\
-    "punpcklbw " mm(7)", " dst"\n\t"
-
-#define PABS(tmp, dst) \
-    "pxor    " tmp ", " tmp"\n\t"\
-    "psubw   " dst ", " tmp"\n\t"\
-    "pmaxsw  " tmp ", " dst"\n\t"
-
-#define CHECK(pj, mj) \
-    _movu " "#pj"(%[curr],%[mrefs]), " mm(2)"\n\t"\
-    _movu " "#mj"(%[curr],%[prefs]), " mm(3)"\n\t"\
-    _mova "    " mm(2)", " mm(4)"\n\t"\
-    _mova "    " mm(2)", " mm(5)"\n\t"\
-    "pxor      " mm(3)", " mm(4)"\n\t"\
-    "pavgb     " mm(3)", " mm(5)"\n\t"\
-    "pand      %[pb1],   " mm(4)"\n\t"\
-    "psubusb   " mm(4)", " mm(5)"\n\t"\
-    RSHIFT(      1,        mm(5))\
-    "punpcklbw " mm(7)", " mm(5)"\n\t"\
-    _mova "    " mm(2)", " mm(4)"\n\t"\
-    "psubusb   " mm(3)", " mm(2)"\n\t"\
-    "psubusb   " mm(4)", " mm(3)"\n\t"\
-    "pmaxub    " mm(3)", " mm(2)"\n\t"\
-    _mova "    " mm(2)", " mm(3)"\n\t"\
-    _mova "    " mm(2)", " mm(4)"\n\t"\
-    RSHIFT(      1,        mm(3))\
-    RSHIFT(      2,        mm(4))\
-    "punpcklbw " mm(7)", " mm(2)"\n\t"\
-    "punpcklbw " mm(7)", " mm(3)"\n\t"\
-    "punpcklbw " mm(7)", " mm(4)"\n\t"\
-    "paddw     " mm(3)", " mm(2)"\n\t"\
-    "paddw     " mm(4)", " mm(2)"\n\t"
-
-#define CHECK1 \
-    _mova "    " mm(0)", " mm(3)"\n\t"\
-    "pcmpgtw   " mm(2)", " mm(3)"\n\t"\
-    "pminsw    " mm(2)", " mm(0)"\n\t"\
-    _mova "    " mm(3)", " mm(6)"\n\t"\
-    "pand      " mm(3)", " mm(5)"\n\t"\
-    "pandn     " mm(1)", " mm(3)"\n\t"\
-    "por       " mm(5)", " mm(3)"\n\t"\
-    _mova "    " mm(3)", " mm(1)"\n\t"
-
-/* pretend not to have checked dir=2 if dir=1 was bad. hurts both quality and speed, but matches the C version. */
-#define CHECK2 \
-    "paddw     %[pw1],   " mm(6)"\n\t"\
-    "psllw        $14,   " mm(6)"\n\t"\
-    "paddsw    " mm(6)", " mm(2)"\n\t"\
-    _mova "    " mm(0)", " mm(3)"\n\t"\
-    "pcmpgtw   " mm(2)", " mm(3)"\n\t"\
-    "pminsw    " mm(2)", " mm(0)"\n\t"\
-    "pand      " mm(3)", " mm(5)"\n\t"\
-    "pandn     " mm(1)", " mm(3)"\n\t"\
-    "por       " mm(5)", " mm(3)"\n\t"\
-    _mova "    " mm(3)", " mm(1)"\n\t"
-
-#define FILTER(prev2, next2, alignment) \
-    while (dest < destEnd)\
-    {\
-        asm volatile\
-        (\
-            "pxor      " mm(7)", " mm(7)"\n\t"\
-            LOAD("(%[curr],%[mrefs])", mm(0))\
-            LOAD("(%[curr],%[prefs])", mm(1))\
-            LOAD("(%["#prev2"])",  mm(2))\
-            LOAD("(%["#next2"])",  mm(3))\
-            _mova "    " mm(3)", " mm(4)"\n\t"\
-            "paddw     " mm(2)", " mm(3)"\n\t"\
-            "psraw     $1,       " mm(3)"\n\t"\
-            _mova "    " mm(0)", %[tmp0]\n\t"\
-            _mova "    " mm(3)", %[tmp1]\n\t"\
-            _mova "    " mm(1)", %[tmp2]\n\t"\
-            "psubw     " mm(4)", " mm(2)"\n\t"\
-            PABS(        mm(4),    mm(2))\
-            LOAD("(%[prev],%[mrefs])", mm(3))\
-            LOAD("(%[prev],%[prefs])", mm(4))\
-            "psubw     " mm(0)", " mm(3)"\n\t"\
-            "psubw     " mm(1)", " mm(4)"\n\t"\
-            PABS(        mm(5),    mm(3))\
-            PABS(        mm(5),    mm(4))\
-            "paddw     " mm(4)", " mm(3)"\n\t"\
-            "psrlw     $1,       " mm(2)"\n\t"\
-            "psrlw     $1,       " mm(3)"\n\t"\
-            "pmaxsw    " mm(3)", " mm(2)"\n\t"\
-            LOAD("(%[next],%[mrefs])", mm(3))\
-            LOAD("(%[next],%[prefs])", mm(4))\
-            "psubw     " mm(0)", " mm(3)"\n\t"\
-            "psubw     " mm(1)", " mm(4)"\n\t"\
-            PABS(        mm(5),    mm(3))\
-            PABS(        mm(5),    mm(4))\
-            "paddw     " mm(4)", " mm(3)"\n\t"\
-            "psrlw     $1,       " mm(3)"\n\t"\
-            "pmaxsw    " mm(3)", " mm(2)"\n\t"\
-            _mova "    " mm(2)", %[tmp3]\n\t"\
-\
-            "paddw     " mm(0)", " mm(1)"\n\t"\
-            "paddw     " mm(0)", " mm(0)"\n\t"\
-            "psubw     " mm(1)", " mm(0)"\n\t"\
-            "psrlw     $1,       " mm(1)"\n\t"\
-            PABS(        mm(2),    mm(0))\
-\
-            _movu " -1(%[curr],%[mrefs]), " mm(2)"\n\t"\
-            _movu " -1(%[curr],%[prefs]), " mm(3)"\n\t"\
-            _mova "             " mm(2)", " mm(4)"\n\t"\
-            "psubusb            " mm(3)", " mm(2)"\n\t"\
-            "psubusb            " mm(4)", " mm(3)"\n\t"\
-            "pmaxub             " mm(3)", " mm(2)"\n\t"\
-            SH(                   mm(2),    mm(3))\
-            "punpcklbw          " mm(7)", " mm(2)"\n\t"\
-            "punpcklbw          " mm(7)", " mm(3)"\n\t"\
-            "paddw              " mm(2)", " mm(0)"\n\t"\
-            "paddw              " mm(3)", " mm(0)"\n\t"\
-            "psubw               %[pw1],  " mm(0)"\n\t"\
-\
-            CHECK(-2,0)\
-            CHECK1\
-            CHECK(-3,1)\
-            CHECK2\
-            CHECK(0,-2)\
-            CHECK1\
-            CHECK(1,-3)\
-            CHECK2\
-\
-            /* Spatial check */ \
-            _mova "    %[tmp3],  " mm(6)" \n\t"\
-            "cmpl      $1, %[spatialCheck]\n\t"\
-            "jne       1f\n\t"\
-            LOAD("(%["#prev2"],%[mrefs],2)", mm(2))\
-            LOAD("(%["#next2"],%[mrefs],2)", mm(4))\
-            LOAD("(%["#prev2"],%[prefs],2)", mm(3))\
-            LOAD("(%["#next2"],%[prefs],2)", mm(5))\
-            "paddw     " mm(4)", " mm(2)"\n\t"\
-            "paddw     " mm(5)", " mm(3)"\n\t"\
-            "psrlw     $1,       " mm(2)"\n\t"\
-            "psrlw     $1,       " mm(3)"\n\t"\
-            _mova "    %[tmp0],  " mm(4)"\n\t"\
-            _mova "    %[tmp1],  " mm(5)"\n\t"\
-            _mova "    %[tmp2],  " mm(7)"\n\t"\
-            "psubw     " mm(4)", " mm(2)"\n\t"\
-            "psubw     " mm(7)", " mm(3)"\n\t"\
-            _mova "    " mm(5)", " mm(0)"\n\t"\
-            "psubw     " mm(4)", " mm(5)"\n\t"\
-            "psubw     " mm(7)", " mm(0)"\n\t"\
-            _mova "    " mm(2)", " mm(4)"\n\t"\
-            "pminsw    " mm(3)", " mm(2)"\n\t"\
-            "pmaxsw    " mm(4)", " mm(3)"\n\t"\
-            "pmaxsw    " mm(5)", " mm(2)"\n\t"\
-            "pminsw    " mm(5)", " mm(3)"\n\t"\
-            "pmaxsw    " mm(0)", " mm(2)"\n\t"\
-            "pminsw    " mm(0)", " mm(3)"\n\t"\
-            "pxor      " mm(4)", " mm(4)"\n\t"\
-            "pmaxsw    " mm(3)", " mm(6)"\n\t"\
-            "psubw     " mm(2)", " mm(4)"\n\t"\
-            "pmaxsw    " mm(4)", " mm(6)"\n\t"\
-\
-            "1:\n\t"\
-            _mova "    %[tmp1],  " mm(2)"\n\t"\
-            _mova "    " mm(2)", " mm(3)"\n\t"\
-            "psubw     " mm(6)", " mm(2)"\n\t"\
-            "paddw     " mm(6)", " mm(3)"\n\t"\
-            "pmaxsw    " mm(2)", " mm(1)"\n\t"\
-            "pminsw    " mm(3)", " mm(1)"\n\t"\
-            "packuswb  " mm(1)", " mm(1)"\n\t"\
-\
-            _movh "    " mm(1)",  %[dest]\n\t"\
-\
-            :[tmp0]        "=m"(tmp0),\
-             [tmp1]        "=m"(tmp1),\
-             [tmp2]        "=m"(tmp2),\
-             [tmp3]        "=m"(tmp3),\
-             [dest]        "=m"(*dest)\
-            :[prev]         "r"(prev),\
-             [curr]         "r"(curr),\
-             [next]         "r"(next),\
-             [prefs]        "r"(prefs),\
-             [mrefs]        "r"(mrefs),\
-             [pw1]          "m"(pw_1),\
-             [pb1]          "m"(pb_1),\
-             [spatialCheck] "g"(spatialCheck)\
-        );\
-        dest  += alignment;\
-        prev  += alignment;\
-        curr  += alignment;\
-        next  += alignment;\
-    }
-
-#ifdef QMPLAY2_CPU_X86_32 //Every x86-64 CPU has SSE2, so MMXEXT is unused there
-static void filterLine_MMXEXT(quint8 *dest, const void *const destEnd,
-                              const quint8 *prev, const quint8 *curr, const quint8 *next,
-                              const qptrdiff prefs, const qptrdiff mrefs,
-                              const int spatialCheck, const bool filterParity)
-{
-    const quint64 pw_1(0x0001000100010001ULL);
-    const quint64 pb_1(0x0101010101010101ULL);
-    quint64 tmp0, tmp1, tmp2, tmp3;
-
-    #define _mova  "movq"
-    #define _movu  "movq"
-    #define _movh  "movd"
-    #define  mm(n) "%%mm"#n
-
-    #define RSHIFT(val, dst) \
-        "psrlq   $"#val"*8, " dst"\n\t"
-
-    #define SH(src, dst) \
-        "pshufw $9, " src", " dst"\n\t"
-
-    if (filterParity)
-        FILTER(prev, curr, 4)
-    else
-        FILTER(curr, next, 4)
-
-    asm volatile("emms");
-
-    #undef SH
-    #undef RSHIFT
-    #undef mm
-    #undef _movh
-    #undef _movu
-    #undef _mova
-}
-#endif // QMPLAY2_CPU_X86_32
-static void filterLine_SSE2(quint8 *dest, const void *const destEnd,
-                            const quint8 *prev, const quint8 *curr, const quint8 *next,
-                            const qptrdiff prefs, const qptrdiff mrefs,
-                            const int spatialCheck, const bool filterParity)
-{
-    struct u128
-    {
-        u128() = default;
-        inline u128(quint64 v) : lo(v), hi(v) {}
-        quint64 lo, hi;
-    };
-    __attribute__((aligned(16))) const u128 pw_1(0x0001000100010001ULL);
-    __attribute__((aligned(16))) const u128 pb_1(0x0101010101010101ULL);
-    __attribute__((aligned(16))) u128 tmp0, tmp1, tmp2, tmp3;
-
-    #define _mova  "movdqa"
-    #define _movu  "movdqu"
-    #define _movh  "movq"
-    #define  mm(n) "%%xmm"#n
-
-    #define RSHIFT(val, dst) \
-        "psrldq   $"#val", " dst"\n\t"
-
-    #define SH(src, dst) \
-        _mova " " src",    " dst"\n\t"\
-        "psrldq   $2,      " dst"\n\t"
-
-    if (filterParity)
-        FILTER(prev, curr, 8)
-    else
-        FILTER(curr, next, 8)
-
-    #undef SH
-    #undef RSHIFT
-    #undef mm
-    #undef _movh
-    #undef _movu
-    #undef _mova
-}
-
-#undef LOAD
-#undef PABS
-#undef CHECK
-#undef CHECK1
-#undef CHECK2
-#undef FILTER
-
-#endif // QMPLAY2_CPU_X86
-static void filterLine_CPP(quint8 *dest, const void *const destEnd,
-                           const quint8 *prev, const quint8 *curr, const quint8 *next,
-                           const qptrdiff prefs, const qptrdiff mrefs,
-                           const int spatialCheck, const bool filterParity)
-{
-    filterLine<true>(dest, destEnd, prev, curr, next, prefs, mrefs, spatialCheck, filterParity);
 }
 
 static void filterSlice(const int plane, const int parity, const int tff, const bool spatialCheck,
@@ -427,7 +132,7 @@ static void filterSlice(const int plane, const int parity, const int tff, const 
     const quint8 *const nextData = nextFrame.constData(plane);
     quint8 *const destData = destFrame.data(plane);
 
-    const int toSub = 3 + alignment - 1;
+    constexpr int toSub = 3;
 
     for (int y = sliceStart; y < sliceEnd; ++y)
     {
@@ -455,7 +160,7 @@ static void filterSlice(const int plane, const int parity, const int tff, const 
                 doSpatialCheck,
                 filterParity
             );
-            filterLinePtr
+            filterLine<true>
             (
                 dest  + 3,
                 dest  + w - toSub,
@@ -507,26 +212,6 @@ YadifDeint::YadifDeint(bool doubler, bool spatialCheck)
     , m_spatialCheck(spatialCheck)
 {
     m_threadsPool.setMaxThreadCount(min(QThread::idealThreadCount(), 18));
-    if (!filterLinePtr)
-    {
-        filterLinePtr = filterLine_CPP;
-        alignment = 1;
-#ifdef QMPLAY2_CPU_X86
-        const int cpuFlags = QMPlay2CoreClass::getCPUFlags();
-        if (cpuFlags & AV_CPU_FLAG_SSE2)
-        {
-            filterLinePtr = filterLine_SSE2;
-            alignment = 8;
-        }
-#ifdef QMPLAY2_CPU_X86_32
-        else if (cpuFlags & AV_CPU_FLAG_MMXEXT)
-        {
-            filterLinePtr = filterLine_MMXEXT;
-            alignment = 4;
-        }
-#endif // QMPLAY2_CPU_X86_32
-#endif // QMPLAY2_CPU_X86
-    }
     addParam("DeinterlaceFlags");
     addParam("W");
     addParam("H");
