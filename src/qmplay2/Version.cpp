bfdff240dd15e52c5c85b0a1f8fb898424b5f93b
Błażej Szczygieł <mumei6102@gmail.com>
2023-09-05 19:10:03 +0200

    Update ChangeLog and version to 23.09.05

--- src/qmplay2/Version.cpp
+++ src/qmplay2/Version.cpp
@@ -25,7 +25,7 @@
 #ifndef QMPlay2GitHEAD
     #define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "23.08.22" QMPlay2GitHEAD
+#define QMPlay2Version "23.09.05" QMPlay2GitHEAD
 
 QByteArray Version::get()
 {
