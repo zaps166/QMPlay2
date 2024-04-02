f1e5bd81a6d1a097946dde25b5d6078c39d15e0b
Błażej Szczygieł <mumei6102@gmail.com>
2024-04-02 17:51:58 +0200

    Update ChangeLog and version to 24.04.02

--- src/qmplay2/Version.cpp
+++ src/qmplay2/Version.cpp
@@ -25,7 +25,7 @@
 #ifndef QMPlay2GitHEAD
     #define QMPlay2GitHEAD
 #endif
-#define QMPlay2Version "24.03.16" QMPlay2GitHEAD
+#define QMPlay2Version "24.04.02" QMPlay2GitHEAD
 
 QByteArray Version::get()
 {
