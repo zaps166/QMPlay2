445b2e45357e847368119146f1e6685d627c08db
Badr <badrazizi3@gmail.com>
2024-12-23 07:03:32 +0300

    update ar.ts

--- lang/ar.ts
+++ lang/ar.ts
@@ -257,7 +257,7 @@
     </message>
     <message>
         <source>&amp;Custom</source>
-        <translation type="unfinished"></translation>
+        <translation>مخصص</translation>
     </message>
 </context>
 <context>
@@ -2066,7 +2066,7 @@
     </message>
     <message>
         <source>No subtitles found for the specified language</source>
-        <translation type="unfinished"></translation>
+        <translation>لم يتم العثور على ترجمات للغة المحددة</translation>
     </message>
 </context>
 <context>
@@ -2279,11 +2279,11 @@
     </message>
     <message>
         <source>Custom aspect ratio</source>
-        <translation type="unfinished"></translation>
+        <translation>تخصيص أبعاد الشاشة</translation>
     </message>
     <message>
         <source>Set aspect ratio</source>
-        <translation type="unfinished"></translation>
+        <translation>تعيين أبعاد الشاشة</translation>
     </message>
 </context>
 <context>
