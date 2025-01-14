208adf8c8c75da63a197e69d0b1dba15baa1c143
Błażej Szczygieł <mumei6102@gmail.com>
2025-01-14 19:31:06 +0100

    YouTube: Add new live stream formats

--- src/modules/Extensions/YouTube.cpp
+++ src/modules/Extensions/YouTube.cpp
@@ -718,7 +718,7 @@ void YouTube::setItags(int qualityIdx)
         AV1_HFR_HIGH_1080p = 699,
         AV1_HFR_HIGH_720p = 698,
 
-        // Live video
+        // Live video (old)
         H264_144p_AAC_48 = 91,
         H264_240p_AAC_48 = 92,
         H264_360p_AAC_128 = 93,
@@ -727,6 +727,20 @@ void YouTube::setItags(int qualityIdx)
         H264_1080p_AAC_256 = 96,
         H264_720p60_AAC_128 = 300,
         H264_1080p60_AAC_128 = 301,
+
+        // Live video (new)
+        H264_144p_LIVE = 269,
+        H264_240p_LIVE = 229,
+        H264_360p_LIVE = 230,
+        H264_480p_LIVE = 231,
+        H264_720p_LIVE = 232,
+        H264_1080p_LIVE = 270,
+        H264_720p60_LIVE = 311,
+        H264_1080p60_LIVE = 312,
+
+        // Live video audio (new)
+        AAC_48_Live = 233,
+        AAC_128_Live = 234,
 
         // Audio
         Opus_160 = 251,
@@ -814,6 +828,21 @@ void YouTube::setItags(int qualityIdx)
             qualityPresets[Preset_4320p60] << VP9_4320p60 << qualityPresets[Preset_2160p60];
         }
 
+        // Append live streams
+        {
+            qualityPresets[Preset_480p]  << H264_480p_LIVE << H264_360p_LIVE << H264_240p_LIVE << H264_144p_LIVE;
+            qualityPresets[Preset_720p]  << H264_720p_LIVE << qualityPresets[Preset_480p];
+            qualityPresets[Preset_1080p] << H264_1080p_LIVE << qualityPresets[Preset_720p];
+            qualityPresets[Preset_1440p] << qualityPresets[Preset_1080p];
+            qualityPresets[Preset_2160p] << qualityPresets[Preset_1440p];
+
+            qualityPresets[Preset_720p60]  << H264_720p60_LIVE;
+            qualityPresets[Preset_1080p60] << H264_1080p60_LIVE << qualityPresets[Preset_720p60];
+            qualityPresets[Preset_1440p60] << qualityPresets[Preset_1080p60];
+            qualityPresets[Preset_2160p60] << qualityPresets[Preset_1440p60];
+            qualityPresets[Preset_4320p60] << qualityPresets[Preset_2160p60];
+        }
+
         // Append also non-60 FPS itags to 60 FPS itags
         qualityPresets[Preset_720p60]  << qualityPresets[Preset_720p];
         qualityPresets[Preset_1080p60] << qualityPresets[Preset_1080p];
@@ -823,7 +852,7 @@ void YouTube::setItags(int qualityIdx)
     }
 
     QVector<int> liveQualityPresets[PresetCount];
-    {
+    { // XXX: Is it still needed?
         liveQualityPresets[Preset_480p]  << H264_480p_AAC_128 << H264_360p_AAC_128 << H264_240p_AAC_48 << H264_144p_AAC_48;
         liveQualityPresets[Preset_720p]  << H264_720p_AAC_256 << liveQualityPresets[Preset_480p];
         liveQualityPresets[Preset_1080p] << H264_1080p_AAC_256 << liveQualityPresets[Preset_720p];
@@ -846,7 +875,7 @@ void YouTube::setItags(int qualityIdx)
 
     QMutexLocker locker(&m_itagsMutex);
     m_videoItags = qualityPresets[qualityIdx];
-    m_audioItags = {Opus_160, AAC_128};
+    m_audioItags = {Opus_160, AAC_128, AAC_128_Live, AAC_48_Live};
     m_hlsItags = liveQualityPresets[qualityIdx];
 }
 
